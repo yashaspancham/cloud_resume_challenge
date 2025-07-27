fca30c6d9a2b237e1b121754c9d59ffeb38a8624
Yashas <yashaspancham@gmail.com>
2025-07-27 17:46:50 +0530

    Yashas-feat-DoneForNow

--- app/page.tsx
+++ app/page.tsx
@@ -4,45 +4,52 @@ import WorkExperience from "@/components/WorkExperience";
 import CertificationsAndAchievements from "@/components/CertificationsAndAchievements";
 import PersonalProjects from "@/components/PersonalProjects";
 import ContactMe from "@/components/ContactMe";
+import FireBall from "@/components/Fireball";
+
 
 export default function Home() {
   return (
     <main className="max-md:p-10 max-md:flex max-md:flex-col max-md:gap-20 bg-black text-white w-full h-full scroll-smooth">
-      <section className="mt-20 md:mx-20 md:p-20 md:broder-[white] md:border-t md:border-l md:border-r md:rounded-t-xl">
+      <section className="2xl:h-[40vw] xl:h-[65vw] lg:h-[100vw] md:h-[110vw] sm:h-[120vw] h-[200vw] m-0 sm:m-10 sticky top-0 z-0">
+      <FireBall />
+      </section>
+      <section className="mt-20 md:mx-20 md:p-20 ">
         <Info />
       </section>
       <section
         id="tech-stack"
-        className="md:mx-20 md:p-20 md:broder-[white] md:border-l md:border-r md:flex md:justify-end"
+        className="md:mx-20 md:p-20  md:flex md:justify-end"
       >
         <TechStack />
       </section>
       <section
         id="work-experience"
-        className="md:mx-20 md:p-20 md:broder-[white] md:border-l md:border-r md:flex"
+        className="md:mx-20 md:p-20  md:flex"
       >
         <WorkExperience />
       </section>
       <section
         id="certifications-and-achievements"
-        className="md:mx-20 md:p-20 md:broder-[white] md:border-l md:border-r md:flex md:justify-end"
+        className="md:mx-20 md:p-20  md:flex md:justify-end"
       >
         <CertificationsAndAchievements />
       </section>
       <section
         id="personal-projects"
-        className="md:mx-20 md:p-20 md:broder-[white] md:border-l md:border-r"
+        className="md:mx-20 md:p-20 "
       >
         <PersonalProjects />
       </section>
       <section
         id="contact-me"
-        className="md:mx-20 md:p-20 broder-[white] md:border-l md:border-r md:border-b md:flex md:justify-end md:rounded-b-xl"
+        className="md:mx-20 md:p-20   md:flex md:justify-end"
       >
         <ContactMe />
       </section>
-      <section className="h-[500px] flex justify-center items-center text-md" >
+      <section className="h-[500px] w-full flex justify-center items-center text-md relative z-10" >
+        <p>
         Thank you for you time and consideration!
+        </p>
       </section>
     </main>
   );
