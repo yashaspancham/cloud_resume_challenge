02d65d31732dfc536feb406b8360584ac8c54925
Yashas <yashaspancham@gmail.com>
2025-11-24 21:29:51 +0530

    Yashas-feat-TechStackUpgrade

---
+++ projectsData/techStackData.ts
@@ -0,0 +1,50 @@
+export const techLogos = [
+    // Frontend
+    { techName: "Next.js", src: "/assets/logo/nextjs.png", alt: "nextjs logo", catagory: ["Frontend"] },
+    { techName: "React", src: "/assets/logo/react.png", alt: "react logo", catagory: ["Frontend"] },
+    { techName: "Javascript", src: "/assets/logo/java-script-logo.png", alt: "java-script logo", catagory: ["Frontend", "Backend"] },
+    { techName: "Typescript", src: "/assets/logo/typescript.png", alt: "typescript logo", catagory: ["Frontend", "Backend"] },
+    { techName: "HTML", src: "/assets/logo/html.png", alt: "html logo", catagory: ["Frontend"] },
+    { techName: "CSS", src: "/assets/logo/css.png", alt: "css logo", catagory: ["Frontend"] },
+    { techName: "Tailwind CSS", src: "/assets/logo/tailwind-css.png", alt: "tailwindcss logo", catagory: ["Frontend"] },
+    { techName: "GSAP", src: "/assets/logo/gsap-logo.png", alt: "GSAP logo", catagory: ["Frontend"] },
+    { techName: "3.js", src: "/assets/logo/threejs-logo.png", alt: "3.js logo", catagory: ["Frontend"] },
+
+    // DevOps
+    { techName: "Python", src: "/assets/logo/python-logo.png", alt: "python logo", catagory: ["DevOps", "Backend"] },
+    { techName: "Linux", src: "/assets/logo/tux.png", alt: "linux logo", catagory: ["DevOps"] },
+    { techName: "NGINX", src: "/assets/logo/nginx.png", alt: "nginx logo", catagory: ["DevOps"] },
+    { techName: "Git", src: "/assets/logo/gitLogo.png", alt: "git logo", catagory: ["DevOps"] },
+    { techName: "Github", src: "/assets/logo/github.png", alt: "github logo", catagory: ["DevOps"] },
+    { techName: "Docker", src: "/assets/logo/docker.png", alt: "docker logo", catagory: ["DevOps"] },
+    { techName: "Kubernetes", src: "/assets/logo/k8s.png", alt: "k8s logo", catagory: ["DevOps"] },
+
+    // CloudOps
+    { techName: "S3", src: "/assets/logo/Simple Storage Service.png", alt: "S3 logo", catagory: ["CloudOps"] },
+    { techName: "Route 53", src: "/assets/logo/Route 53.png", alt: "Route 53 logo", catagory: ["CloudOps"] },
+    { techName: "EC2", src: "/assets/logo/EC2.png", alt: "EC2 logo", catagory: ["CloudOps"] },
+    { techName: "Lambda", src: "/assets/logo/lambdaIcon.png", alt: "Lambda logo", catagory: ["CloudOps"] },
+    { techName: "CloudFront", src: "/assets/logo/cloudfrontIcon.png", alt: "CloudFront logo", catagory: ["CloudOps"] },
+    { techName: "IAM", src: "/assets/logo/IAMIcon.jpeg", alt: "IAM logo", catagory: ["CloudOps"] },
+    { techName: "API Gateway", src: "/assets/logo/APIGatewayIcon.jpeg", alt: "API Gateway logo", catagory: ["CloudOps"] },
+    { techName: "DynamoDB", src: "/assets/logo/dynamoDBIcon.jpeg", alt: "DynamoDB logo", catagory: ["CloudOps"] },
+
+    // Backend
+    //djangoIcon.png
+    { techName: "Django", src: "/assets/logo/djangoIcon.png", alt: "django logo", catagory: ["Backend"] },
+    { techName: "Node.js", src: "/assets/logo/nodejs.png", alt: "node logo", catagory: ["Backend"] },
+    { techName: "SQL", src: "/assets/logo/sql-server.png", alt: "sql logo", catagory: ["Backend"] },
+    { techName: "Strapi", src: "/assets/logo/strapiLogo.png", alt: "strapi logo", catagory: ["Backend"] },
+
+    // Tools / AI
+    { techName: "Cursor", src: "/assets/logo/cursorLogo.jpeg", alt: "Cursor logo", catagory: ["AI tools"] },
+];
+
+export const techStackCategories = [
+    "All",
+    "Frontend",
+    "Backend",
+    "DevOps",
+    "CloudOps",
+    // "AI tools",
+];
